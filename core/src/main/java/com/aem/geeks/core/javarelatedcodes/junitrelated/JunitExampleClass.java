44f2f5297cd345fc51692f7a3235361872d9dc80
bhargavseshadri <bhargavseshadri2389@gmail.com>
2026-02-18 19:49:46 +0530

    Junit Commit

---
+++ core/src/main/java/com/aem/geeks/core/javarelatedcodes/junitrelated/JunitExampleClass.java
@@ -0,0 +1,30 @@
+package com.aem.geeks.core.javarelatedcodes.junitrelated;
+
+// Calculator.java
+public class JunitExampleClass {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int divide(int a, int b) {
+        return a / b;
+    }
+
+    public String getMessage() {
+        return "Hello";
+    }
+
+    public String getNullValue() {
+        return null;
+    }
+
+    public int[] getNumbers() {
+        return new int[]{1, 2, 3};
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+}
