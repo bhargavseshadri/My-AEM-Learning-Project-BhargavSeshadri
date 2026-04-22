3139f9d5b1a403d4015c2b8f0c1202c286fc0984
bhargav seshadri <bhargavseshadri2389@gmail.com>
2026-04-22 20:05:17 +0530

    bhargav-backend-aem-apis-comp Sling model commit

--- core/src/main/java/com/aem/geeks/core/models/BhargavBackendAemApisCompModel.java
+++ core/src/main/java/com/aem/geeks/core/models/BhargavBackendAemApisCompModel.java
@@ -1,10 +1,16 @@
 package com.aem.geeks.core.models;
 
+import org.apache.sling.api.resource.Resource;
+
 public interface BhargavBackendAemApisCompModel {
 
     String getPersonName();
     String getFatherName();
     String getAverageIncome();
     String[] getMultifieldvalues();
+    String getResourcePath();
+    String getResourceDetails();
 
+
+    String getValueMapDetails();
 }
--- core/src/main/java/com/aem/geeks/core/models/impl/BhargavBackendAemApisCompModelImpl.java
+++ core/src/main/java/com/aem/geeks/core/models/impl/BhargavBackendAemApisCompModelImpl.java
@@ -4,12 +4,15 @@ package com.aem.geeks.core.models.impl;
 import com.aem.geeks.core.models.BhargavBackendAemApisCompModel;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
 @Model(adaptables = {Resource.class, SlingHttpServletRequest.class}, adapters = BhargavBackendAemApisCompModel.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class BhargavBackendAemApisCompModelImpl implements BhargavBackendAemApisCompModel {
+
 
 
     //Printing The Basic Dialog Fields Data
@@ -44,4 +47,68 @@ public class BhargavBackendAemApisCompModelImpl implements BhargavBackendAemApis
     public String[] getMultifieldvalues() {
         return multifieldvalues;
     }
+
+
+
+
+/***********************************************USAGE OF BACKEND API's**********************/
+
+/*_______________Using Resource
+
+  Resource : Current Resource - Means where am I in the repository */
+    @SlingObject
+    private Resource resource;
+
+    @Override
+    public String getResourcePath() {
+        return "Resource Path : " + resource.getPath();
+    }
+
+    @Override
+    public String getResourceDetails() {
+        return "Resource Name : " + resource.getName() + "\n" + "Resource Type : " + resource.getResourceType() + "\n" + "Parent Resource Details : " + resource.getParent()  + "\n" + "Parent Resource Path : " + resource.getParent().getPath();
+    }
+
+
+/*_______________Using VALUEMAP
+
+ ValueMap : It only gives the properties which are present in the current resource.*/
+
+//    In Sling Model we can directly get the ValueMap like this also.
+//    @Inject
+//    private ValueMap valueMap;
+
+
+
+    @Override
+    public String getValueMapDetails() {
+
+        //  Getting ValueMap using "Resource"
+        ValueMap valueMap = resource.getValueMap();
+
+        //we can print this prop value like this or like above using annotation.
+        String personNameUsingValueMap = valueMap.get("personName", String.class);
+        String lastModifiedBy = valueMap.get("jcr:lastModifiedBy", String.class);
+
+        return "Person Name printing using ValueMap : " + personNameUsingValueMap + "\n" + "lastModifiedBy : " + lastModifiedBy ;
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
