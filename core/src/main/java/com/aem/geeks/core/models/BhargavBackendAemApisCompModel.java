72ea3add00db77d5752febddcb54ba43e74461dd
bhargav seshadri <bhargavseshadri2389@gmail.com>
2026-04-24 01:39:27 +0530

    bhargav-backend-aem-apis-comp Sling model commit

--- core/src/main/java/com/aem/geeks/core/models/BhargavBackendAemApisCompModel.java
+++ core/src/main/java/com/aem/geeks/core/models/BhargavBackendAemApisCompModel.java
@@ -2,9 +2,13 @@ package com.aem.geeks.core.models;
 
 import org.apache.sling.api.resource.Resource;
 
+import javax.annotation.PostConstruct;
 import java.util.List;
 
 public interface BhargavBackendAemApisCompModel {
+
+    @PostConstruct
+    String postConstructMethod();
 
     String getPersonName();
     String getFatherName();
@@ -13,6 +17,12 @@ public interface BhargavBackendAemApisCompModel {
     String getResourcePath();
     String getResourceDetails();
     String isExpectedResourceType();
-//    List<Resource> getChildrenList();
+    List<Resource> getChildrenList();
     String getValueMapDetails();
+
+    String getCurrentPageDetails();
+
+    String getPropsUsingValueMap();
+
+    List<String> childPageDetails();
 }
--- core/src/main/java/com/aem/geeks/core/models/impl/BhargavBackendAemApisCompModelImpl.java
+++ core/src/main/java/com/aem/geeks/core/models/impl/BhargavBackendAemApisCompModelImpl.java
@@ -2,22 +2,23 @@ package com.aem.geeks.core.models.impl;
 
 
 import com.aem.geeks.core.models.BhargavBackendAemApisCompModel;
+import com.day.cq.wcm.api.Page;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ValueMap;
 import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.ScriptVariable;
 import org.apache.sling.models.annotations.injectorspecific.SlingObject;
 import org.apache.sling.models.annotations.injectorspecific.ValueMapValue;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 @Model(adaptables = {Resource.class, SlingHttpServletRequest.class}, adapters = BhargavBackendAemApisCompModel.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
 public class BhargavBackendAemApisCompModelImpl implements BhargavBackendAemApisCompModel {
-
-
 
     //Printing The Basic Dialog Fields Data
     @ValueMapValue
@@ -31,6 +32,13 @@ public class BhargavBackendAemApisCompModelImpl implements BhargavBackendAemApis
 
     @ValueMapValue
     private String[] multifieldvalues;
+
+    @PostConstruct
+    @Override
+    public String postConstructMethod(){
+        return "Post Construct Method is Executed";
+    }
+
 
     @Override
     public String getPersonName() {
@@ -53,13 +61,12 @@ public class BhargavBackendAemApisCompModelImpl implements BhargavBackendAemApis
     }
 
 
-
-
 /***********************************************USAGE OF BACKEND API's**********************/
 
-/*_______________Using Resource
-
+/*_______________Using Resource____________________
   Resource : Current Resource - Means where am I in the repository */
+
+    //We can get the Resource using @ScriptVariable & @Inject also, by @SlingObject is preferrable
     @SlingObject
     private Resource resource;
 
@@ -70,36 +77,44 @@ public class BhargavBackendAemApisCompModelImpl implements BhargavBackendAemApis
 
     @Override
     public String getResourceDetails() {
-        return "Resource Name : " + resource.getName() + "\n" + "Resource Type : " + resource.getResourceType() + "\n" + "Parent Resource Details : " + resource.getParent()  + "\n" + "Parent Resource Path : " + resource.getParent().getPath();
+        return "Resource Name : " + resource.getName() + "\n" + "Resource Type : " + resource.getResourceType() + "\n"
+                + "Parent Resource Details : " + resource.getParent() + "\n" + "Parent Resource Path : " + resource.getParent().getPath();
     }
 
     @Override
     public String isExpectedResourceType() {
-        return "aemgeeks/components/bhargav-backend-aem-apis-comp - Is this the Sling:resourceType : " + resource.isResourceType("aemgeeks/components/bhargav-backend-aem-apis-comp");
+        return "aemgeeks/components/bhargav-backend-aem-apis-comp - Is this the Sling:resourceType : " +
+                resource.isResourceType("aemgeeks/components/bhargav-backend-aem-apis-comp");
     }
 
-    //Getting hold of child resources  -- This code wont print anything now because the current resource has no chilred, so use this code in resourceResolver case by getting parent resoyrce
-//    @Override
-//    public List<Resource> getChildrenList() {
-//
-//        List<Resource> childrenList = new ArrayList<>();
-//        Iterator<Resource> iterator = resource.listChildren();
-//
-//        while (iterator.hasNext()) {
-//            Resource child = iterator.next();
-//            childrenList.add(child);
-//        }
-//        return childrenList;
-//    }
+//Getting hold of child resources
+    @Override
+    public List<Resource> getChildrenList() {
 
-/*_______________Using VALUEMAP
+        List<Resource> childrenList = new ArrayList<>();
+        String parentPath = resource.getParent().toString();
 
- ValueMap : It only gives the properties which are present in the current resource. And it cant get hold of the props that are present in parent resource*/
+        //Here I am going to the Parent because my Current Page have no Children.
+        Iterator<Resource> iterator = resource.getParent().listChildren();
 
-//    In Sling Model we can directly get the ValueMap like this also.
-//    @Inject
-//    private ValueMap valueMap;
+        while (iterator.hasNext()) {
+            Resource child = iterator.next();
+            childrenList.add(child);
+        }
+        return childrenList;
+    }
 
+
+
+
+
+    /*_______________Using VALUEMAP____________________
+     ValueMap : It only gives the properties which are present in the current resource. And it can't get hold of the props that
+                are present in parent or child resources*/
+
+    //    In Sling Model we can directly get the ValueMap like this also.
+    //    @Inject
+    //    private ValueMap valueMap;
 
     @Override
     public String getValueMapDetails() {
@@ -111,7 +126,70 @@ public class BhargavBackendAemApisCompModelImpl implements BhargavBackendAemApis
         String personNameUsingValueMap = valueMap.get("personName", String.class);
         String lastModifiedBy = valueMap.get("jcr:lastModifiedBy", String.class);
 
-        return "Person Name printing using ValueMap : " + personNameUsingValueMap + "\n" + "lastModifiedBy : " + lastModifiedBy ;
+        return "Person Name printing using ValueMap : " + personNameUsingValueMap + "\n" + "lastModifiedBy : " + lastModifiedBy;
+    }
+
+
+
+
+
+    /*_______________Using @ScriptVariable____________________
+     *
+     * ScriptVariable : using this we can get Page, PageManager, Resource, ResourceResolver, SlingHttpServletRequest,
+     *                  SlingHttpServletResponse, ComponentContext, WCMMode  */
+
+    //Getting Page Object
+    @ScriptVariable
+    private Page currentPage;
+
+    @Override
+    public String getCurrentPageDetails() {
+
+        //Printing JCR:Title of a Page
+        String jcrTitle = currentPage.getTitle();
+
+        //Here we are directly getting hold of JCR:Content of the page
+        String contentResource = currentPage.getContentResource().getPath();
+
+        String pagePath = currentPage.getPath();
+        String parentPath = currentPage.getParent().getPath();
+        String templateTitle = currentPage.getTemplate().getTitle();
+        String templatePath = currentPage.getTemplate().getPath();
+        String language = currentPage.getLanguage().toString();
+
+        // This will only print, If we give Page Title in the "Page Properties" of a page. Jcr:Title and this are not same.
+        String pageTitle = currentPage.getPageTitle();
+
+        return "Jcr:Title : " + jcrTitle + "\n" + "Page Path : " + pagePath + "\n" + "Parent Path : " + parentPath + "\n" + "Page Title : "
+                + pageTitle + "\n" + "Template Title : " + templateTitle + "\n" + "Template Path : " + templatePath + "\n"
+                + "JCR Content Resource Path : : " + contentResource + "\n" + "Language of the Page : " + language;
+
+    }
+
+    @Override
+    public String getPropsUsingValueMap() {
+
+        //Getting a ValueMap using Page Object and printing the Properties of the Page.
+        ValueMap allPageProperties = currentPage.getProperties();
+        String jcrTitle = allPageProperties.get("jcr:title", String.class);
+        String cqTemplate = allPageProperties.get("cq:template", String.class);
+
+        return "Jcr:Title : " + jcrTitle + "\n" + "cqTemplate : " + cqTemplate;
+
+    }
+
+    @Override
+    public List<String> childPageDetails() {
+
+        List<String> childPagesList = new ArrayList<>();
+
+        //Here We are going to "/content/aemgeeks/us/en"(Parent) from our current page and then we print all childs of "EN".
+        Iterator<Page> childrenPages = currentPage.getParent().listChildren();
+        while (childrenPages.hasNext()){
+            childPagesList.add(childrenPages.next().getPath());
+        }
+
+        return childPagesList;
     }
 
 }
