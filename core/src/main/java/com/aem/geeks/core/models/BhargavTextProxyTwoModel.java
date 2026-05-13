adeeb3cb168c98c5e1dd5c31408f7f2773798a45
bhargav seshadri <bhargavseshadri2389@gmail.com>
2026-05-13 16:57:33 +0530

    commit

---
+++ core/src/main/java/com/aem/geeks/core/models/BhargavTextProxyTwoModel.java
@@ -0,0 +1,55 @@
+package com.aem.geeks.core.models;
+
+
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.models.annotations.Model;
+import com.adobe.cq.wcm.core.components.models.Text;
+import org.apache.sling.models.annotations.Via;
+import org.apache.sling.models.annotations.injectorspecific.Self;
+import org.apache.sling.models.annotations.via.ResourceSuperType;
+
+@Model(
+        adaptables = {Resource.class,SlingHttpServletRequest.class},
+        adapters = Text.class,
+        resourceType = "aemgeeks/components/bhargavtextproxy-two" //this is important for this model tto work.
+)
+public class BhargavTextProxyTwoModel implements Text{
+
+    @Self
+    @Via(type = ResourceSuperType.class)  //here it is going to the core text component and getting this property
+    private Text textDelegate;  //@Via(ResourceSuperType.class) means  Adapt the super resource type instead of current component.
+
+    @Override
+    public String getText() {
+
+        String originalText = textDelegate.getText();
+
+        if (originalText != null) {
+            return originalText + " - This is added by using dellegation, Bhargav";
+        }
+
+        return null;
+    }
+
+
+    /*
+        Remaining methods delegated manually
+     */
+
+    @Override
+    public boolean isRichText() {
+        return textDelegate.isRichText();
+    }
+
+    @Override
+    public String getId() {
+        return textDelegate.getId();
+    }
+
+    @Override
+    public String getExportedType() {
+        return textDelegate.getExportedType();
+    }
+
+}
