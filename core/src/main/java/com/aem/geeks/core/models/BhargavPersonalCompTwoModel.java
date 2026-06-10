94cb5e576dc8d3633a204019674d2ea01aadfcc6
bhargav seshadri <bhargavseshadri2389@gmail.com>
2026-06-10 18:23:13 +0530

    Component Connection commit

---
+++ core/src/main/java/com/aem/geeks/core/models/BhargavPersonalCompTwoModel.java
@@ -0,0 +1,9 @@
+package com.aem.geeks.core.models;
+
+public interface BhargavPersonalCompTwoModel {
+    String getManufacturerName();
+
+    String getProductName();
+
+    String getProductPrice();
+}
---
+++ core/src/main/java/com/aem/geeks/core/models/impl/BhargavPersonalCompTwoModelImpl.java
@@ -0,0 +1,52 @@
+package com.aem.geeks.core.models.impl;
+
+import com.aem.geeks.core.models.BhargavPersonalCompTwoModel;
+import org.apache.sling.api.SlingHttpServletRequest;
+import org.apache.sling.api.resource.Resource;
+import org.apache.sling.api.resource.ResourceResolver;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
+import org.apache.sling.models.annotations.Model;
+import org.apache.sling.models.annotations.injectorspecific.SlingObject;
+
+import javax.annotation.PostConstruct;
+
+
+
+/*BhargavSeshadri : Step: 3 - Now to fetch the comp A dialog values in comp B using Sling model
+*
+*
+Step: 1 - We should have a component A with some dialog fields. : apps/aemgeeks/components/content/bhargav-personalcomp-one
+Step: 2 - Create a component B where we want to use the component A dialog values. : apps/aemgeeks/components/content/bhargav-personalcomp-two
+* */
+@Model(adaptables = {Resource.class, SlingHttpServletRequest.class}, adapters = BhargavPersonalCompTwoModel.class, defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL)
+public class BhargavPersonalCompTwoModelImpl implements BhargavPersonalCompTwoModel{
+
+    @SlingObject
+    private ResourceResolver resourceResolver;
+
+    @SlingObject
+    private Resource resource;
+
+    @PostConstruct
+    protected void init() {
+        //We have authored the comp-a : apps/aemgeeks/components/content/bhargav-personalcomp-one in this page/resource
+        resource = resourceResolver.getResource("/content/aemgeeks/us/en/imp-page---bhargav/jcr:content/root/responsivegrid/bhargav_personalcomp");
+    }
+
+    @Override
+    public String getManufacturerName() {
+        return resource.getValueMap().get("manufacturerName", String.class);
+    }
+
+    @Override
+    public String getProductName() {
+        return resource.getValueMap().get("productName", String.class);
+    }
+
+    @Override
+    public String getProductPrice() {
+        return resource.getValueMap().get("productPrice", String.class);
+    }
+
+
+}
