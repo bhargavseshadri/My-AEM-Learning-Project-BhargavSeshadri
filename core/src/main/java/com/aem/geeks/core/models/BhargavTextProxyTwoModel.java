089325bcadc2aa3ad19909e75c16f520344746a4
bhargav seshadri <bhargavseshadri2389@gmail.com>
2026-07-21 18:00:20 +0530

    Random commit

--- core/src/main/java/com/aem/geeks/core/models/BhargavTextProxyTwoModel.java
+++ core/src/main/java/com/aem/geeks/core/models/BhargavTextProxyTwoModel.java
@@ -1,10 +1,10 @@
 package com.aem.geeks.core.models;
 
 
+import com.adobe.cq.wcm.core.components.models.Text;
 import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.models.annotations.Model;
-import com.adobe.cq.wcm.core.components.models.Text;
 import org.apache.sling.models.annotations.Via;
 import org.apache.sling.models.annotations.injectorspecific.Self;
 import org.apache.sling.models.annotations.via.ResourceSuperType;
@@ -13,22 +13,22 @@ import org.apache.sling.models.annotations.via.ResourceSuperType;
 
 
 /*BhargavSeshadri - Step: 2 (last step)- Sling Model
-*
-* For Step: 1 - Create Component : apps/aemgeeks/components/bhargavtextproxy-two*/
+ *
+ * For Step: 1 - Create Component : apps/aemgeeks/components/bhargavtextproxy-two*/
 
 @Model(
-        adaptables = {Resource.class,SlingHttpServletRequest.class},
+        adaptables = {Resource.class, SlingHttpServletRequest.class},
         adapters = Text.class,
         resourceType = "aemgeeks/components/bhargavtextproxy-two" //this is important for this model tto work.
 )
-public class BhargavTextProxyTwoModel implements Text{
+public class BhargavTextProxyTwoModel implements Text {
 
     @Self
     @Via(type = ResourceSuperType.class)  //here it is going to the core text component and getting this property
     private Text textDelegate;  //@Via(ResourceSuperType.class) means  Adapt the super resource type instead of current component.
 
     @Override
-    public String getText() {
+    public String getText() {  //here we are, "kind of" overriding the getText method and adding our logic
 
         String originalText = textDelegate.getText();
 
@@ -37,26 +37,6 @@ public class BhargavTextProxyTwoModel implements Text{
         }
 
         return null;
-    }
-
-
-    /*
-        Remaining methods delegated manually
-     */
-
-    @Override
-    public boolean isRichText() {
-        return textDelegate.isRichText();
-    }
-
-    @Override
-    public String getId() {
-        return textDelegate.getId();
-    }
-
-    @Override
-    public String getExportedType() {
-        return textDelegate.getExportedType();
     }
 
 }
