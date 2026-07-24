04e4fea1c7b87134cd0529ff76bb06ab1453c9bd
bhargav seshadri <bhargavseshadri2389@gmail.com>
2026-07-24 13:33:43 +0530

    random commit

---
+++ core/src/main/java/com/aem/geeks/core/javarelatedcodes/Rough.java
@@ -0,0 +1,13 @@
+package com.aem.geeks.core.javarelatedcodes;
+
+import java.util.*;
+
+public class Rough {
+
+        public static void iterateArrayList(Iterator iterator) {
+
+
+            // Just print the elements, don't provide new line
+
+    }
+}
