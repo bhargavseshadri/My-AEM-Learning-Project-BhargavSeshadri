7d191387e03612f9d52ac2f9054334249d3c37ff
bhargav seshadri <bhargavseshadri2389@gmail.com>
2026-07-28 01:43:47 +0530

    Random commit

--- core/src/main/java/com/aem/geeks/core/javarelatedcodes/Rough.java
+++ core/src/main/java/com/aem/geeks/core/javarelatedcodes/Rough.java
@@ -3,11 +3,27 @@ package com.aem.geeks.core.javarelatedcodes;
 import java.util.*;
 
 public class Rough {
+    public static void main(String[] args) {
+        TreeSet<Integer> bhaList = new TreeSet<>();
 
-        public static void iterateArrayList(Iterator iterator) {
+        bhaList.add(6);
+        bhaList.add(5);
+        bhaList.add(3);
+        bhaList.add(4);
+        bhaList.add(2);
+        bhaList.add(1);
 
 
-            // Just print the elements, don't provide new line
+        Iterator<Integer> it = bhaList.iterator();
+        while (it.hasNext()){
+            Integer temp = it.next();
+            System.out.println("temp : "+temp);
+        }
 
+
+
+
+//        System.out.println("List : "+bhaList);
+//        System.out.println("Set : "+setting);
     }
 }
