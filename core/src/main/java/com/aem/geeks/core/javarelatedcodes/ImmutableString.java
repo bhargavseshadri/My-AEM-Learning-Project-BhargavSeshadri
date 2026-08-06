af03debc8da235bf45de71f4c0312502b7fa52e6
bhargav seshadri <bhargavseshadri2389@gmail.com>
2026-08-06 09:31:58 +0530

    random commit

---
+++ core/src/main/java/com/aem/geeks/core/javarelatedcodes/ImmutableString.java
@@ -0,0 +1,17 @@
+package com.aem.geeks.core.javarelatedcodes;
+
+public class ImmutableString {
+    public static void main(String[] args) {
+
+        String s1 = "Hello";
+        String s2 = s1;
+
+        s1 = s1.concat(" World");
+
+        System.out.println("s1 = " + s1);
+        System.out.println("s2 = " + s2);
+
+
+        //at first both the string variables points to the same literals but after concat s1 points to  a new literral where as s2 still points to s1
+    }
+}
